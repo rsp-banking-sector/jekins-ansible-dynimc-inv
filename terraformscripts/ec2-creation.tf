ba690dcbf5ffd87932a036d62921c50f42f4ae9c
rajeshreddy1999 <114590578+rajeshreddy1999@users.noreply.github.com>
2023-07-02 19:22:46 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,16 +1,14 @@
-provider "aws"{
-region = "eu-west-3"
-}
-
-resource "aws_instance" "AWSServer"{
-ami = "ami-0d767e966f3458eb5"
-instance_type = "t2.micro"
-key_name = "devopsmss"
-security_groups = ["launch-wizard-2"]
-tags={
-Name = "tomcatservers"
-}
-}
-
+provider = "aws" {
+	region = "ap-south-1"
+	}
+	resource "aws_instance" "AWSInstance" {
+	 ami="ami-008b85aa3ff5c1b02"
+	 instance_type = "t2.micro"
+	 key_name = "devopsmss"
+	 security_groups = ["launch-wizard-2"]
+	 tags={
+	 Name = "tomcatservers"
+	 }
+	 }
 
 
